b571eed8b71e9251206a17909b37ab04e584ccd7
Hameed Abdul <hameedabdulrashid@gmail.com>
2017-02-12 10:56:35 -0600

    v2

---
+++ Assets/Scripts/BallExample.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallExample : MonoBehaviour {
+
+    public PlayerController playerController;
+    public PlatformCameraFollow platCam;
+    public Camera cam;
+    public GameObject iceBall;
+    public Rigidbody2D rb;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine("CinematicExample");
+        rb.isKinematic = false;
+        //cam.transform.position += offset * Time.deltaTime;
+    }
+
+
+    IEnumerator CinematicExample()
+    {
+
+        Vector3 offset = new Vector3(3,0);
+
+        playerController.enabled = false;
+        platCam.enabled = false;
+        Vector3.MoveTowards(cam.transform.position, (cam.transform.position += offset),offset.sqrMagnitude);
+
+        yield return new WaitForSeconds(8f);
+        playerController.enabled = true;
+        platCam.enabled = true;
+    }
+}
---
+++ Assets/Scripts/SlipperyIceMechanic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlipperyIceMechanic : MonoBehaviour {
+
+
+    public Vector2 slipperyIce = new Vector2(1,0);
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        slipperyIce = IceFactor(other.GetComponent<Rigidbody2D>());
+        other.GetComponent<Rigidbody2D>().AddForce(slipperyIce, ForceMode2D.Force);
+    }
+	
+
+    Vector2 IceFactor(Rigidbody2D rb)
+    {
+        Vector2 iceFactor = new Vector3(0,0,0);
+        if (rb.velocity.sqrMagnitude >= new Vector2(0, 0).sqrMagnitude)
+            iceFactor = new Vector2(2, 0);
+        else if (rb.velocity.sqrMagnitude <= new Vector2(-.1f, 0).sqrMagnitude)
+            iceFactor = new Vector2(-2, 0);
+        return iceFactor;
+    }
+}
